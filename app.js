7137d4cf05857da7eb146849c08817e85acf3d44
Milo MacPhail <milomacphail@gmail.com>
2018-08-08 16:45:02 +0000

    Lesson 4

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const wmf = document.querySelector('#book-list li:nth-child(2).name');
-//console.log(wmf);
-
-var books = document.querySelector('#book-list li.name');
-//console.log(books)
-
-books = document.querySelectorAll('#book-list li.name');
+var books = document.querySelectorAll('#book-list li.name');
 console.log(books);
-
-Array.from(books).forEach(function(book) {
-    console.log(book);
-});
