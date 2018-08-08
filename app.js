4c7ce47332fc650acb0d1d972e613e42e0205940
Milo MacPhail <milomacphail@gmail.com>
2018-08-08 17:29:04 +0000

    Lesson 6 completed

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-var books = document.querySelectorAll('#book-list li.name');
-console.log(books);
+const banner = document.querySelector('#page-banner');
 
+console.log('#page-banner node type is:'.banner.nodeType);
+console.log('#page-banner node name is:'.banner.nodeName);
+console.log('#page-banner has child nodes:'.banner.hasChildNodes());
 
-Array.from(books).forEach(function(book){
-   book.textContent += '(book.title)';
-});
-
-const bookList = document.querySelector('#book-list');
-bookList.innerHTML = '<h2>Books and more books...</h2>'
-
-bookList.innerHTML += '<p>This is how you add HTML</p>';  
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
