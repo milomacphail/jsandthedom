60eb0f4890ba14880b0aa7cab9e91433d6d63233
Milo MacPhail <milomacphail@gmail.com>
2018-08-09 22:26:22 +0000

    Lesson 9 completed

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const bookList = document.querySelector('#book-list');
+var btns = document.querySelectorAll('#book-list.delete');
 
-console.log('book-list next sibling is:', bookList.nextSibling);
-console.log('book-list next element sibling is:', bookList.nextElementSibling);
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', function(e){
+       
+       
+       const li = e.target.parentElement;
+       
+       li.parentNode.removeChild(li);
+    });
+});
+,
+const link = document.querySelector('#page-banner a');
 
-console.log('book-list previous sibling is:', bookList.previousSibling);
-console.log('book-list previous element sibling is:', bookList.previousElementSibling);
-
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br/> Too cool for everyone else!'
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('navigation to', .e.target.textContent, " was prevented");
+})
