feeeb702230cc981ac65314f1bf24b3b751427bc
Milo MacPhail <milomacphail@gmail.com>
2018-08-10 01:36:58 +0000

    Lesson 16

--- app.js
+++ app.js
@@ -26,19 +26,19 @@ hideBox.addEventListener('change', function(e){
 });
 
 //filter books
-const searchBar = document.forms['search-books'].querySelector("input");
+const searchBar = document.forms['search-books'].querySelector('input');
 searchBar.addEventListener('keyup', function(e){
-  const term = e.target.value.toLowerCase()
+  const term = e.target.value.toLowerCase();
   const books = list.getElementsByTagName('li');
   Array.from(books).forEach(function(book){
     const title = book.firstElementChild.textContent;
-    if(title.toLowerCase().indexOf(term) !=-1 ){
-      book.style.display = "block";
+    if(title.toLowerCase().indexOf(term) != -1 ){
+      book.style.display ='block';
     } else {
-      book.style.display = "none";
+      book.style.display = 'none';
     }
-  })
-})
+  });
+});
 
 //create elements
 
